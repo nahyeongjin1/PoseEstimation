31af53d3a238f614c466d682e13af9573b739cd5
nahyeongjin1 <71560837+nahyeongjin1@users.noreply.github.com>
2021-01-23 22:12:17 +0900

    Add files via upload

---
+++ CHAPTER08/09.affine_event.py
@@ -0,0 +1,46 @@
+import numpy as np, cv2
+
+def contain_pts(p, p1, p2):
+    return p1[0] <= p[0] < p2[0] and p1[1] <= p[1] < p2[1]
+
+def draw_rect(title, img, pts):
+    global small
+    rois = [(p - small, small * 2) for p in pts]
+    for (x, y), (w, h) in np.int32(rois):
+        cv2.rectangle(img, (x, y, w, h), (0, 255, 0), 2)
+    cv2.imshow(title, img)
+
+def affine(img):
+    global pts1, pts2
+    aff_mat = cv2.getAffineTransform(pts1, pts2)
+    dst = cv2.warpAffine(img, aff_mat, img.shape[1::-1], cv2.INTER_LINEAR)
+    draw_rect('image', np.copy(img), pts1)
+    draw_rect('dst', dst, pts2)
+
+def onMouse(event, x, y, flags, param):
+    global pts1, pts2, small
+    global check
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for i, p in enumerate(pts1):
+            p1, p2 = p - small, p + small
+            if contain_pts((x, y), p1, p2): check = i
+
+    if event == cv2.EVENT_LBUTTONUP: check = -1
+
+    if check >= 0:
+        pts1[check] = (x, y)
+        affine(np.copy(image))
+
+small = np.array([12, 12])
+pts1 = np.float32([(30, 30), (450, 30), (200, 370)])
+pts2 = np.float32([(30, 30), (450, 30), (200, 370)])
+
+image = cv2.imread("images/affine1.jpg", 1)
+if image is None: raise Exception("영상파일 읽기 에러")
+
+check = -1
+
+draw_rect('image', np.copy(image), pts1)
+draw_rect('dst', np.copy(image), pts2)
+cv2.setMouseCallback("image", onMouse, 0)
+cv2.waitKey(0)
